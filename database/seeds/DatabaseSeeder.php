891562d4bea3bc6be5b3ddfd5ebdbe5c70e782b3
Sergey <32011415+leguha@users.noreply.github.com>
2019-05-14 13:47:20 +0300

    Добавлены сиды для создания основных ролей и администратора

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Role;
+use App\Models\User;
+use App\Models\Permission;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,46 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        $superAdmin = new Role();
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('x5410041'),
+        ]);
+
+        $adminRole = Role::create([
+            'name' => 'super-admin',
+            'display_name' => 'Главный администратор',
+            'description' => 'Пользователь, который имеет все права.'
+        ]);
+
+        $adminRole->attachPermission(
+            Permission::create([
+                'name' => 'create-dispatcher',
+                'display_name' => 'Создовать диспетчеров',
+                'description' => 'Разрешает пользователю создовать диспетчеров.'
+            ])
+        );
+
+        $role = $adminRole->getKey();
+
+        if(is_array($role)) {
+            $role = $role['id'];
+        }
+
+        $admin->roles()->attach($role);
+
+//        $admin->attchRole($adminRole);
+
+        Role::create([
+            'name' => 'dispatcher',
+            'display_name' => 'Диспетчер',
+            'description' => 'Пользователь, который ответственен за принятие заявок.'
+        ])->attachPermission(
+            Permission::create([
+                'name' => 'accept-applications',
+                'display_name' => 'Принимать заявки',
+                'description' => 'Разрешает пользователю принимать заявки, поступающие от клиентов.'
+            ])
+        );
     }
 }
