3dbe5cc81c2953e709a4c79163a53b717d4247c8
Sergey <32011415+leguha@users.noreply.github.com>
2019-05-15 14:40:27 +0300

    Добавлена миграция для мест.

---
+++ database/migrations/2019_05_14_141315_create_place_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlaceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('place', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('block');
+            $table->integer('floor');
+            $table->integer('row');
+            $table->string('place-number');
+            $table->string('status');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('place');
+    }
+}
