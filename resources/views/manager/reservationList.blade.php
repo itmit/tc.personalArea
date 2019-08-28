b7d589576a584413fc84341d644c2c6845ea189e
Bronyrus <bronyruschannel@gmail.com>
2019-08-28 17:59:44 +0300

    Update reservationList.blade.php

--- resources/views/manager/reservationList.blade.php
+++ resources/views/manager/reservationList.blade.php
@@ -29,7 +29,7 @@
                 <td>{{ $place->place()->floor }}</td>
                 <td>{{ $place->place()->row }}</td>
                 <td>{{ $place->place()->place_number }}</td>
-                <td><button class="makeReservation"  data-userid="{{ $place->id }}" data-placeid="{{ $place->place_id }}">Забронировать</button> / <button class="cancelReservation" data-placeid="{{ $place->place_id }}">Отказать</button></td>
+                <td><button class="makeReservation"  data-userid="{{ $place->id }}" data-placeid="{{ $place->place_id }}">Забронировать</button> / <button class="cancelReservation" data-placeid="{{ $place->id }}">Отказать</button></td>
             </tr>
         @endforeach
         </tbody>
