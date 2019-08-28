971d7fc9cd62fc3baeafd71ae07c671fe73be055
Bronyrus <bronyruschannel@gmail.com>
2019-08-28 11:44:57 +0300

    Update ReservationWebController.php

--- app/Http/Controllers/Web/ReservationWebController.php
+++ app/Http/Controllers/Web/ReservationWebController.php
@@ -6,6 +6,7 @@ use App\Models\Place;
 use App\Models\Reservation;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class ReservationWebController extends Controller
 {
