f62b3c32066c50953742b0406d690307f5b8178c
Bronyrus <bronyruschannel@gmail.com>
2019-08-28 11:17:25 +0300

    Update reservationList.blade.php

--- resources/views/manager/reservationList.blade.php
+++ resources/views/manager/reservationList.blade.php
@@ -29,10 +29,43 @@
                 <td>{{ $place->place()->floor }}</td>
                 <td>{{ $place->place()->row }}</td>
                 <td>{{ $place->place()->place_number }}</td>
-            <td><button class="makeReservation" data-placeid=" {{ $place->place_id }} ">Забронировать</button></td>
+            <td><button class="makeReservation" data-placeid="{{ $place->place_id }}">Забронировать</button></td>
             </tr>
         @endforeach
         </tbody>
     </table>
 
+    <script>
+        $(document).ready(function()
+        {
+            $(document).on('click', '.makeReservation', function() {
+            let place_id = $(this).data('placeid');
+            console.log(place_id);
+            // $.ajax({
+            //     headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+            //     dataType: "json",
+            //     data: {selectBidsByStatus: selectBidsByStatus},
+            //     url     : 'bid/updateList',
+            //     method    : 'post',
+            //     success: function (response) {
+            //         let result = '';
+            //             for(var i = 0; i < response.length; i++) {
+            //                 result += '<tr>';
+            //                 result += '<td><a href="bid/' + response[i]['id'] + '">' + response[i]['status'] + '</a></td>';
+            //                 result += '<td>' + response[i]['client']['email'] + '</td>';
+            //                 result += '<td>' + response[i]['location']['latitude'] + ' | ' + response[i]['location']['longitude'] + '</td>';
+            //                 result += '<td>' + response[i]['type'] + '</td>';
+            //                 result += '<td>' + response[i]['created_at'] + '</td>';
+            //                 result += '<td>' + response[i]['updated_at'] + '</td>';
+            //                 result += '</tr>';
+            //             }
+            //             $('tbody').html(result);
+            //     },
+            //     error: function (xhr, err) { 
+            //         console.log(err + " " + xhr);
+            //     }
+            // });
+            });
+        });
+    </script>
 @endsection
