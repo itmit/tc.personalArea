9131bd2ce0c81f37edeb57ee9cc4fa269e1be084
Bronyrus <bronyruschannel@gmail.com>
2019-09-06 16:21:07 +0300

    Update reservationList.blade.php

--- resources/views/manager/reservationList.blade.php
+++ resources/views/manager/reservationList.blade.php
@@ -125,7 +125,7 @@
             $(document).on('click', '.cancelReservation', function() {
                 let place_id = $(this).data('placeid');
                 let user_id = $(this).data('userid');
-                // console.log(place_id + ' ' + user_id);
+                console.log(place_id + ' ' + user_id);
                 $.ajax({
                     headers : {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
                     dataType: "json",
