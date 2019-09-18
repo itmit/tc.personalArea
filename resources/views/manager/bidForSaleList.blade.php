a110fcf9f71d9f1970bd00be1881593e51ab22f9
Bronyrus <bronyruschannel@gmail.com>
2019-09-18 17:30:23 +0300

    Update bidForSaleList.blade.php

--- resources/views/manager/bidForSaleList.blade.php
+++ resources/views/manager/bidForSaleList.blade.php
@@ -5,6 +5,9 @@
     <table class="table table-bordered">
         <thead>
         <tr>
+            <th>Блок</th>
+            <th>Этаж</th>
+            <th>Ряд</th>
             <th>Место</th>
             <th>Имя продавца</th>
             <th>Номер телефона</th>
@@ -13,6 +16,9 @@
         <tbody>
         @foreach($bids as $bid)
             <tr>
+                <td>{{ $bid->place()->get()->first()->block }}</td>
+                <td>{{ $bid->place()->get()->first()->floor }}</td>
+                <td>{{ $bid->place()->get()->first()->row }}</td>
                 <td>{{ $bid->place()->get()->first()->place_number }}</td>
                 <td>{{ $bid->seller_name }}</td>
                 <td>{{ $bid->phone_number }}</td>
