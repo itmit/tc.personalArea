45ea58dec8a66fb996a8e6e76b9a4bd7869423ce
Bronyrus <bronyruschannel@gmail.com>
2019-10-09 12:07:15 +0300

    Update news.blade.php

--- resources/views/news/news.blade.php
+++ resources/views/news/news.blade.php
@@ -16,7 +16,7 @@
                 <h1>{{ $newsItem->head }}</h1>
             <div class="row">
             <div class="col-8 col-sm-6">
-                <img src="{{ $newsItem->picture }}" alt="{{ $newsItem->head }}" width="100%" height="100%">
+                <img src="{{ $newsItem->picture }}" alt="{{ $newsItem->head }}" width="100%" height="100%" style="float:left; margin: 7px 7px 7px 0;">                  }>
             </div>
             <div class="col-4 col-sm-6">
                 {!! htmlspecialchars_decode($newsItem->body) !!}
