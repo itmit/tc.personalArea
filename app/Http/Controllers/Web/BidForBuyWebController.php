05a9442ac2cfecf9b3150ede524b065864fdadef
Bronyrus <bronyruschannel@gmail.com>
2019-10-17 16:58:15 +0300

    Update BidForBuyWebController.php

--- app/Http/Controllers/Web/BidForBuyWebController.php
+++ app/Http/Controllers/Web/BidForBuyWebController.php
@@ -22,7 +22,7 @@ class BidForBuyWebController extends Controller
     {
         return view('manager.bidForBuyList', [
             'title' => 'Заявки на сдачу в аренду помещения',
-            'bids' => BidForBuy::with('place')->get()
+            'bids' => BidForBuy::with('place')->orderBy('created_at', 'desc')->get()
         ]);
     }
 
