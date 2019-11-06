2daf0731b3f3d895522c673667fdaaba6eaba819
Bronyrus <bronyruschannel@gmail.com>
2019-11-06 16:38:42 +0300

    история заявок на бронирование

---
+++ database/migrations/2019_11_06_122843_create_reservation_histories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation_histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('bid')->unsigned();
+            $table->enum('action', ['создание', 'бронирование', 'отмена клиентом', 'отмена менеджером', 'завершено']);
+            $table->integer('timer')->nullable();
+            $table->timestamps();
+
+            $table->foreign('bid')->references('id')->on('reservation');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservation_histories');
+    }
+}
