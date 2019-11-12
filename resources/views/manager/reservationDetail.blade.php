4793b221b0193ebadb2961878ef841cae14dcdd9
Bronyrus <bronyruschannel@gmail.com>
2019-11-12 15:53:50 +0300

    Update reservationDetail.blade.php

--- resources/views/manager/reservationDetail.blade.php
+++ resources/views/manager/reservationDetail.blade.php
@@ -8,7 +8,7 @@
     </div>
     <div class="col-sm-12">
         <div>
-            Заявку создал {{ $reservation->first_name }} {{ $reservation->last_name }} ({{ $reservation->client()->phone }}) в {{ date('H:i:s d.m.Y', strtotime($reservation->created_at->timezone('Europe/Moscow'))) }}
+            Заявку создал <a href="../client/{{ $reservation->client()->id }}">{{ $reservation->first_name }} {{ $reservation->last_name }}</a> ({{ $reservation->client()->phone }}) в {{ date('H:i:s d.m.Y', strtotime($reservation->created_at->timezone('Europe/Moscow'))) }}
         </div>
         <div>
             История заявки
