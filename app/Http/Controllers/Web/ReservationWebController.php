45dccae2d6398aee33fc71edc6549007bb3232de
Bronyrus <bronyruschannel@gmail.com>
2019-11-12 15:29:01 +0300

    Update ReservationWebController.php

--- app/Http/Controllers/Web/ReservationWebController.php
+++ app/Http/Controllers/Web/ReservationWebController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use App\Models\Place;
 use App\Models\Reservation;
+use App\Models\ReservationHistory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
