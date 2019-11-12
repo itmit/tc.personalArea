7697f79427aedc55c018669067e7a713f754ef76
Bronyrus <bronyruschannel@gmail.com>
2019-11-12 15:19:04 +0300

    Update reservationDetail.blade.php

--- resources/views/manager/reservationDetail.blade.php
+++ resources/views/manager/reservationDetail.blade.php
@@ -2,12 +2,14 @@
 
 @section('content')
 
-    <h1 data-reservation-id="{{ $reservation->id }}">Заявка на бронирование</h1>
+    <h1 data-reservation-id="{{ $reservation->id }}">Заявка на бронирование места</h1>
     <div class="col-sm-12">
         <a href="/reservation">Назад</a>
     </div>
     <div class="col-sm-12">
-
+        <div>
+            Заявку создал {{ $reservation->first_name $reservation->last_name}} ({{ $reservation->client()->phone }}) в {{ $reservation->created_at }}
+        </div>
     </div>
 
 @endsection
