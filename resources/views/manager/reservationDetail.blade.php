247a6cef2826ab23f3c69236416f1919135626d8
Bronyrus <bronyruschannel@gmail.com>
2019-11-19 12:01:51 +0300

    Update reservationDetail.blade.php

--- resources/views/manager/reservationDetail.blade.php
+++ resources/views/manager/reservationDetail.blade.php
@@ -70,7 +70,7 @@
     <script>
         $(document).ready(function()
         {
-            let now = new Date();
+            let now = new Date(milliseconds);
             let ends_at = $('.reservation-time-left').data('timer')
             console.log(now + ' ' + ends_at);
 
