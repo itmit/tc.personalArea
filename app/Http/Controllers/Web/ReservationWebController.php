222ea428220ba867bab9c9e17db4317779904df8
Bronyrus <bronyruschannel@gmail.com>
2019-11-19 11:42:46 +0300

    Update ReservationWebController.php

--- app/Http/Controllers/Web/ReservationWebController.php
+++ app/Http/Controllers/Web/ReservationWebController.php
@@ -185,6 +185,7 @@ class ReservationWebController extends Controller
         if($lastAction->action()->type == "reservation")
         {
             $ends_at =strtotime($lastAction->created_at . " + " . $lastAction->timer ." hours");
+            $ends_at = gmdate("Y-m-d H:i:s", $ends_at);
         }
         else
         {
