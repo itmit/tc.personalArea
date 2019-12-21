5215e49867926411bd826b64212046c5e047e2a3
Bronyrus <bronyruschannel@gmail.com>
2019-12-21 10:15:02 +0300

    Update placeList.blade.php

--- resources/views/manager/placeList.blade.php
+++ resources/views/manager/placeList.blade.php
@@ -126,8 +126,8 @@
                         url     : 'places/changePlaceStatus',
                         method    : 'post',
                         success: function (response) {
-                            // console.log(place.html());
-                            place.css("border","3px solid red");
+                            console.log(place.html());
+                            // place.css("border","3px solid red");
                             console.log(response)
                         },
                         error: function (xhr, err) { 
