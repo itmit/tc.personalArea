f1992597b170c14a31e13b503cad6f618965ed52
Bronyrus <bronyruschannel@gmail.com>
2019-12-23 12:11:35 +0300

    Update PlaceApiController.php

--- app/Http/Controllers/Api/PlaceApiController.php
+++ app/Http/Controllers/Api/PlaceApiController.php
@@ -44,7 +44,7 @@ class PlaceApiController extends ApiBaseController
         $action = Actions::where('type', '=', 'reservation')->first();
         $places = Place::select('id', 'block', 'floor', 'row', 'place_number', 'status', 'price')
             ->where('block', '=', $block)
-            ->where('status', '=', 'Свободен')
+            ->where('status', '<>', 'Арендован')
             ->orderBy('sort', 'asc')
             ->offset($request->offset)
             ->limit($request->limit)
