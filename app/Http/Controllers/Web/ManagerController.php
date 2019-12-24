6a3612f6ce67898a1f16fbe4fde9bedd03efa464
Bronyrus <bronyruschannel@gmail.com>
2019-12-24 13:39:45 +0300

    Update ManagerController.php

--- app/Http/Controllers/Web/ManagerController.php
+++ app/Http/Controllers/Web/ManagerController.php
@@ -128,6 +128,6 @@ class ManagerController extends Controller
             'email' => $request->email,
         ]);
         
-        return redirect()->route('admin.managers');
+        return redirect()->route('admin.managers.index');
     }
 }
