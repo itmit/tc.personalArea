d1d55462be682604a5f181042e11a7d746691ad9
Bronyrus <bronyruschannel@gmail.com>
2020-02-03 18:08:38 +0300

    Update WasteWebController.php

--- app/Http/Controllers/Web/WasteWebController.php
+++ app/Http/Controllers/Web/WasteWebController.php
@@ -204,34 +204,34 @@ class WasteWebController extends Controller
 
     private function createExcelUnactive($xls, $response)
     {
-        $xls->setActiveSheetIndex(0);
+        $xls->setActiveSheetIndex(1);
         // Получаем активный лист
         $sheet = $xls->getActiveSheet();
         // Подписываем лист
-        $sheet->setTitle('Активные');
+        $sheet->setTitle('Неактивные');
 
-        // Вставляем текст в ячейку A1
-        $sheet->setCellValue("A1", 'Таблица умножения');
-        $sheet->getStyle('A1')->getFill()->setFillType(
-            PHPExcel_Style_Fill::FILL_SOLID);
-        $sheet->getStyle('A1')->getFill()->getStartColor()->setRGB('EEEEEE');
+        // Вставляем текст в ячейки
+        $sheet->setCellValue("A1", 'Блок');
+        $sheet->setCellValue("B1", 'Этаж');
+        $sheet->setCellValue("C1", 'Ряд');
+        $sheet->setCellValue("D1", 'Место');
+        $sheet->setCellValue("E1", 'Дата освобождения');
+        $sheet->setCellValue("F1", 'Имя');
+        $sheet->setCellValue("G1", 'Телефон');
+        $sheet->setCellValue("H1", 'Статус');
 
-        // Выравнивание текста
-        $sheet->getStyle('A1')->getAlignment()->setHorizontal(
-            PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-
-        for ($i = 2; $i < 10; $i++) {
-            for ($j = 2; $j < 10; $j++) {
-                // Выводим таблицу умножения
-                $sheet->setCellValueByColumnAndRow(
-                                                $i - 2,
-                                                $j,
-                                                $i . "x" .$j . "=" . ($i*$j));
-                // Применяем выравнивание
-                $sheet->getStyleByColumnAndRow($i - 2, $j)->getAlignment()->
-                        setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-            }
-        }
+        // for ($i = 2; $i < 10; $i++) {
+        //     for ($j = 2; $j < 10; $j++) {
+        //         // Выводим таблицу умножения
+        //         $sheet->setCellValueByColumnAndRow(
+        //                                         $i - 2,
+        //                                         $j,
+        //                                         $i . "x" .$j . "=" . ($i*$j));
+        //         // Применяем выравнивание
+        //         $sheet->getStyleByColumnAndRow($i - 2, $j)->getAlignment()->
+        //                 setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+        //     }
+        // }
     }
 
 }
