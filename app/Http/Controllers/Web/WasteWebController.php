8beeb3bd5a1a0e36e62c0e07d491ad8e8320e23d
Bronyrus <bronyruschannel@gmail.com>
2020-02-04 10:22:35 +0300

    Update WasteWebController.php

--- app/Http/Controllers/Web/WasteWebController.php
+++ app/Http/Controllers/Web/WasteWebController.php
@@ -187,7 +187,7 @@ class WasteWebController extends Controller
                 $sheet->setCellValueByColumnAndRow(
                                                 $i,
                                                 $j,
-                                                $response);
+                                                $response['id']);
             }
         }
         return $xls;
