aa6b0fc8ed7975e33bcdb28fd448d27688ae1cfd
Bronyrus <bronyruschannel@gmail.com>
2020-02-29 12:02:05 +0300

    Update placeList.blade.php

--- resources/views/manager/placeList.blade.php
+++ resources/views/manager/placeList.blade.php
@@ -168,7 +168,7 @@
                             success: function (response) {
                                 $('table > tbody  > tr').each(function(index, tr) { 
                                     $(tr).attr('style', 'background-color: #f7ecdd!important');
-                                    $('tr > td > select').val('Арендован')
+                                    $('tr > td > option[value=Арендован]').attr('selected','selected');
                                     // console.log($('tr > td > select > option').val('Арендован'));
                                 });
                             },
